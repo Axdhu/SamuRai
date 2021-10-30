00e24de71e0dbc703081ca79805f936c83bb3ac0
Axdhu <91071377+Axdhu@users.noreply.github.com>
2021-10-30 14:21:22 +0530

    Update afk.py

--- plugins/afk.py
+++ plugins/afk.py
@@ -27,7 +27,7 @@ async def is_afk_(f, client, message):
 is_afk = filters.create(func=is_afk_, name="is_afk_")
 
 
-@friday_on_cmd(
+@samurai_on_cmd(
     ["afk"],
     propagate_to_next_handler=False,
     cmd_help={
