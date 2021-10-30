6b3fca84e283f39a0434b7143bf5c7e1b3ed1eab
Axdhu <91071377+Axdhu@users.noreply.github.com>
2021-10-30 15:40:59 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -4,7 +4,7 @@
 # All rights reserved.
 
 from main_startup.config_var import Config
-from main_startup.core.decorators import friday_on_cmd
+from main_startup.core.decorators import samurai_on_cmd
 from main_startup.core.startup_helpers import run_cmd
 from main_startup.helper_func.basic_helpers import (
     edit_or_reply,
