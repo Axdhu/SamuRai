76fe3fa7b62a3dcd05f4c1301df48eab8c22f75f
Axdhu <91071377+Axdhu@users.noreply.github.com>
2021-10-30 12:51:25 +0530

    Update session.sh

--- resources/session/session.sh
+++ resources/session/session.sh
@@ -2,6 +2,7 @@
 #Copyright (C) 2021 Axdhu/SamuRai
 #This file is a part of < https://github.com/Axdhu/SamuRai >
 #PLease read the GNU Affero General Public License < https://github.com/Axdhu/SamuRai/blob/main/LICENSE >
+# All rights reserved.
 
 clear
 
