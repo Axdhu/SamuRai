1571900af2a439b55b003d77a45153ea6cd961e8
Axdhu <91071377+Axdhu@users.noreply.github.com>
2021-10-31 09:54:40 +0530

    Update tools.py

--- samurai/utils/tools.py
+++ samurai/utils/tools.py
@@ -1,8 +1,9 @@
-# Copyright (C) 2019 Adek Maulana
+#Copyright (C) 2021 Axdhu/SamuRai
 #
-# Licensed under the Raphielscape Public License, Version 1.d (the "License");
-# you may not use this file except in compliance with the License.
+#This file is a part of < https://github.com/Axdhu/SamuRai >
+#PLease read the GNU Affero General Public License < https://github.com/Axdhu/SamuRai/blob/main/LICENSE >
 #
+# All rights reserved.
 
 import re
 import hashlib
