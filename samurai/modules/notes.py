59e9410f838c59e3018f256186228816b664eb3f
Axdhu <91071377+Axdhu@users.noreply.github.com>
2021-10-31 11:33:25 +0530

    Update notes.py

--- samurai/modules/notes.py
+++ samurai/modules/notes.py
@@ -1,12 +1,12 @@
-# Copyright (C) 2019 The Raphielscape Company LLC.
+#Copyright (C) 2021 Axdhu/SamuRai
 #
-# Licensed under the Raphielscape Public License, Version 1.d (the "License");
-# you may not use this file except in compliance with the License.
+#This file is a part of < https://github.com/Axdhu/SamuRai >
+#PLease read the GNU Affero General Public License < https://github.com/Axdhu/SamuRai/blob/main/LICENSE >
 #
 """Userbot module containing commands for keeping notes."""
 
-from userbot import BOTLOG_CHATID, CMD_HELP
-from userbot.events import register
+from samurai import BOTLOG_CHATID, CMD_HELP
+from samurai.events import register
 from asyncio import sleep
 
 
