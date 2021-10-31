1585e1ad80b7d4ca83af434d45b1c2d84f2cacb0
Axdhu <91071377+Axdhu@users.noreply.github.com>
2021-10-31 11:09:45 +0530

    Update anime.py

--- samurai/modules/anime.py
+++ samurai/modules/anime.py
@@ -1,8 +1,9 @@
-# Copyright (C) 2019 The Raphielscape Company LLC.
+#Copyright (C) 2021 Axdhu/SamuRai
 #
-# Licensed under the Raphielscape Public License, Version 1.d (the "License");
-# you may not use this file except in compliance with the License.
+#This file is a part of < https://github.com/Axdhu/SamuRai >
+#PLease read the GNU Affero General Public License < https://github.com/Axdhu/SamuRai/blob/main/LICENSE >
 #
+# All rights reserved.
 
 
 import asyncio
@@ -29,8 +30,8 @@ from telethon.tl.types import (
 )
 from telethon.utils import is_image, is_video
 
-from userbot import CMD_HELP
-from userbot.events import register
+from samurai import CMD_HELP
+from samurai.events import register
 
 jikan = Jikan()
 
