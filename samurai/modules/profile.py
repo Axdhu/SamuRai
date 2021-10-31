9e5a31da7254dcdf08e52b2474864016ea72c613
Axdhu <91071377+Axdhu@users.noreply.github.com>
2021-10-31 11:37:03 +0530

    Update profile.py

--- samurai/modules/profile.py
+++ samurai/modules/profile.py
@@ -1,7 +1,7 @@
-# Copyright (C) 2019 The Raphielscape Company LLC.
+#Copyright (C) 2021 Axdhu/SamuRai
 #
-# Licensed under the Raphielscape Public License, Version 1.d (the "License");
-# you may not use this file except in compliance with the License.
+#This file is a part of < https://github.com/Axdhu/SamuRai >
+#PLease read the GNU Affero General Public License < https://github.com/Axdhu/SamuRai/blob/main/LICENSE >
 #
 """Userbot module for changing your Telegram profile details."""
 
@@ -23,8 +23,8 @@ from telethon.tl.functions.photos import (DeletePhotosRequest,
 
 from telethon.tl.types import InputPhoto, MessageMediaPhoto, User, Chat, Channel
 
-from userbot import bot, CMD_HELP
-from userbot.events import register
+from samurai import bot, CMD_HELP
+from samurai.events import register
 
 # ====================== CONSTANT ===============================
 INVALID_MEDIA = "```The extension of the media entity is invalid.```"
