942b470c9f37251a44c94b5d0bbe91ab3ae67906
Axdhu <91071377+Axdhu@users.noreply.github.com>
2021-10-31 11:41:15 +0530

    Update sed.py

--- samurai/modules/sed.py
+++ samurai/modules/sed.py
@@ -1,7 +1,7 @@
-# Copyright (C) 2019 The Raphielscape Company LLC.
+#Copyright (C) 2021 Axdhu/SamuRai
 #
-# Licensed under the Raphielscape Public License, Version 1.d (the "License");
-# you may not use this file except in compliance with the License.
+#This file is a part of < https://github.com/Axdhu/SamuRai >
+#PLease read the GNU Affero General Public License < https://github.com/Axdhu/SamuRai/blob/main/LICENSE >
 #
 # The entire source code is OSSRPL except 'sed' which is GPLv3
 # License: GPLv3 and OSSRPL
@@ -9,8 +9,8 @@
 
 import re
 from sre_constants import error as sre_err
-from userbot import CMD_HELP
-from userbot.events import register
+from samurai import CMD_HELP
+from samurai.events import register
 
 DELIMITERS = ("/", ":", "|", "_")
 
