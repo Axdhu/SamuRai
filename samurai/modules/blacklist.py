5e9beb26e216545856b222f54b56b3c52204d582
Axdhu <91071377+Axdhu@users.noreply.github.com>
2021-10-31 11:12:14 +0530

    Update blacklist.py

--- samurai/modules/blacklist.py
+++ samurai/modules/blacklist.py
@@ -1,17 +1,19 @@
-# Copyright (C) 2019 The Raphielscape Company LLC.
+#Copyright (C) 2021 Axdhu/SamuRai
 #
-# Licensed under the Raphielscape Public License, Version 1.d (the "License");
-# you may not use this file except in compliance with the License.
+#This file is a part of < https://github.com/Axdhu/SamuRai >
+#PLease read the GNU Affero General Public License < https://github.com/Axdhu/SamuRai/blob/main/LICENSE >
+#
+# All rights reserved.
 
-# port to userbot from uniborg by @keselekpermen69
+# port to userbot from uniborg 
 
 
 import io
 import re
 import userbot.modules.sql_helper.blacklist_sql as sql
 from asyncio import sleep
-from userbot import CMD_HELP
-from userbot.events import register
+from samurai import CMD_HELP
+from samurai.events import register
 
 
 @register(incoming=True, disable_edited=True, disable_errors=True)
