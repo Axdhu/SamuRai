d8100744b60622a179f975c169e5a0d792a21217
Axdhu <91071377+Axdhu@users.noreply.github.com>
2021-10-31 11:35:56 +0530

    Update pics.py

--- samurai/modules/pics.py
+++ samurai/modules/pics.py
@@ -1,12 +1,12 @@
-# Copyright (C) 2019 The Raphielscape Company LLC.
+#Copyright (C) 2021 Axdhu/SamuRai
 #
-# Licensed under the Raphielscape Public License, Version 1.d (the "License");
-# you may not use this file except in compliance with the License.
+#This file is a part of < https://github.com/Axdhu/SamuRai >
+#PLease read the GNU Affero General Public License < https://github.com/Axdhu/SamuRai/blob/main/LICENSE >
 #
 
 from io import BytesIO
-from userbot.events import register
-from userbot import CMD_HELP
+from samurai.events import register
+from samurai import CMD_HELP
 from telethon import types
 from asyncio import sleep
 from telethon.errors import PhotoInvalidDimensionsError
